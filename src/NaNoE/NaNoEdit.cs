8e2572390445e3b935244804c4edd0af5c2766b0
edg3 <loveland.ernest@gmail.com>
2019-08-17 15:29:24 +0200

    editing adjustments

--- src/NaNoE/NaNoEdit.cs
+++ src/NaNoE/NaNoEdit.cs
@@ -52,6 +52,9 @@ namespace NaNoE
         {
             var ans = new List<string>();
             para = para.ToLower();
+            para = para.Replace(",", " ")
+                       .Replace(".", " ")
+                       .Replace(";", " ");
 
             // =========================================================
             // = Checks added                                          =
@@ -70,21 +73,16 @@ namespace NaNoE
                 // if (!SpellCheck(whichUsed)) ans.Add("{" + i.ToString() + "} Spelling Error: " + splt[i]);
             }
 
+            // Side note: this is definitely going to find things that shouldn't be in this - like portions of other words. "is" is in "sister" for example
             // [ replace 'to be' and 'to have' ]
-            if (para.Contains(" to be.")
-             || para.Contains(" to be,")
-             || para.Contains(" to be ")
-             || para.Contains(" to have.")
-             || para.Contains(" to have,")
+            if (para.Contains(" to be ")
              || para.Contains(" to have ")) ans.Add("[" + para.IndexOf("to ") +"]replace 'to be' and 'to have' with something");
             // [ ly = 'He quickly ran across the park.' fixed to 'He darted across the park.' ]
-            if (para.Contains("ly ")
-             || para.Contains("ly,")
-             || para.Contains("ly."))           ans.Add("[" + para.IndexOf("ly") + "]replace '-ly' words with descriptions");
+            if (para.Contains("ly "))           ans.Add("[" + para.IndexOf("ly") + "]replace '-ly' more descriptive: e.g. not 'her eyes were deadly', rather 'with an evil glare she looked at me'");
             /// [ ing = 'I turned and Mary was glaring at me.' fixed to 'I turned and Mary glared.' ]
             if (para.Contains("ing ")
              || para.Contains("ing,")
-             || para.Contains("ing."))          ans.Add("[" + para.IndexOf("ing") + "]replace '-ing' words with minimal words");
+             || para.Contains("ing."))          ans.Add("[" + para.IndexOf("ing") + "]replace '-ing' words with minimal words. e.g. 'she is running daily now' with 'she runs every morning now'");
             // [ -> Begin, begins, began, beginning, start, starts, started, starting = 'he started to run' fixed by 'he ran' ]
             if (para.Contains("begin"))         ans.Add("[" + para.IndexOf("begin") +"]Rather don't use 'begin', simplify it");
             if (para.Contains("begins"))        ans.Add("[" + para.IndexOf("begins") +"]Rather don't use 'begins', simplify it");
@@ -104,28 +102,41 @@ namespace NaNoE
             if (para.Contains("already"))       ans.Add("[" + para.IndexOf("already") +"]Rather minify use 'already', make literal");
             if (para.Contains("finally"))       ans.Add("[" + para.IndexOf("finally") + "]Rather minify use 'finally', make literal");
             // [ See, Look, Hear, Know, Realize, Wonder, Decided, Notice, Feel, Remember, Think, That, Of, Really, Very, Down, Up, Then, Start, begin, Just = reword ]
-            if (para.Contains("see"))           ans.Add("[" + para.IndexOf("see") +"] Rather minify use 'see', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("look"))          ans.Add("[" + para.IndexOf("look") +"] Rather minify use 'look', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("hear"))          ans.Add("[" + para.IndexOf("hear") +"] Rather minify use 'hear', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("know"))          ans.Add("[" + para.IndexOf("know") +"] Rather minify use 'know', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("realize"))       ans.Add("[" + para.IndexOf("realize") +"] Rather minify use 'realize', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("realise"))       ans.Add("[" + para.IndexOf("realise") +"] Rather minify use 'realise', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("wonder"))        ans.Add("[" + para.IndexOf("wonder") +"] Rather minify use 'wonder', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("decided"))       ans.Add("[" + para.IndexOf("decided") +"] Rather minify use 'decided', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("notice"))        ans.Add("[" + para.IndexOf("notice") +"] Rather minify use 'notic', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("feel"))          ans.Add("[" + para.IndexOf("feel") +"] Rather minify use 'feel', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("remember"))      ans.Add("[" + para.IndexOf("remember") +"] Rather minify use 'remember', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("think"))         ans.Add("[" + para.IndexOf("think") +"] Rather minify use 'think', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("that"))          ans.Add("[" + para.IndexOf("that") +"] Rather minify use 'that', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("of"))            ans.Add("[" + para.IndexOf("of") +"] Rather minify use 'of', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("really"))        ans.Add("[" + para.IndexOf("really") +"] Rather minify use 'really', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("very"))          ans.Add("[" + para.IndexOf("very") +"] Rather minify use 'very', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("down"))          ans.Add("[" + para.IndexOf("down") +"] Rather minify use 'down', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("up"))            ans.Add("[" + para.IndexOf("up") +"] Rather minify use 'up', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("then"))          ans.Add("[" + para.IndexOf("then") +"] Rather minify use 'then', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("start"))         ans.Add("[" + para.IndexOf("start") +"] Rather minify use 'start', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("begin"))         ans.Add("[" + para.IndexOf("begin") +"] Rather minify use 'begin', make literal, cant think of another way to mention this TBH");
-            if (para.Contains("just"))          ans.Add("[" + para.IndexOf("just") + "] Rather minify use 'just', make literal, cant think of another way to mention this TBH");
+            if (para.Contains("see"))           ans.Add("[" + para.IndexOf("see") +"] Rather minify use 'see' ");
+            if (para.Contains("look"))          ans.Add("[" + para.IndexOf("look") +"] Rather minify use 'look' ");
+            if (para.Contains("hear"))          ans.Add("[" + para.IndexOf("hear") +"] Rather minify use 'hear' ");
+            if (para.Contains("know"))          ans.Add("[" + para.IndexOf("know") +"] Rather minify use 'know' ");
+            if (para.Contains("realize"))       ans.Add("[" + para.IndexOf("realize") +"] Rather minify use 'realize' ");
+            if (para.Contains("realise"))       ans.Add("[" + para.IndexOf("realise") +"] Rather minify use 'realise' ");
+            if (para.Contains("wonder"))        ans.Add("[" + para.IndexOf("wonder") +"] Rather minify use 'wonder' ");
+            if (para.Contains("decided"))       ans.Add("[" + para.IndexOf("decided") +"] Rather minify use 'decided' ");
+            if (para.Contains("notice"))        ans.Add("[" + para.IndexOf("notice") +"] Rather minify use 'notic' ");
+            if (para.Contains("feel"))          ans.Add("[" + para.IndexOf("feel") +"] Rather minify use 'feel' ");
+            if (para.Contains("remember"))      ans.Add("[" + para.IndexOf("remember") +"] Rather minify use 'remember' ");
+            if (para.Contains("think"))         ans.Add("[" + para.IndexOf("think") +"] Rather minify use 'think' ");
+            if (para.Contains("that"))          ans.Add("[" + para.IndexOf("that") +"] Rather minify use 'that' ");
+            if (para.Contains("of"))            ans.Add("[" + para.IndexOf("of") +"] Rather minify use 'of' ");
+            if (para.Contains("really"))        ans.Add("[" + para.IndexOf("really") +"] Rather minify use 'really', make literal, e.g. 'the swimmer really performed admirably' with 'the swimmer performed admirably'. Slacker descriptive");
+            if (para.Contains("very"))          ans.Add("[" + para.IndexOf("very") +"] Rather minify use 'very'H");
+            if (para.Contains("down"))          ans.Add("[" + para.IndexOf("down") +"] Rather minify use 'down'");
+            if (para.Contains("up"))            ans.Add("[" + para.IndexOf("up") +"] Rather minify use 'up'");
+            if (para.Contains("then"))          ans.Add("[" + para.IndexOf("then") +"] Rather minify use 'then'");
+            if (para.Contains("start"))         ans.Add("[" + para.IndexOf("start") +"] Rather minify use 'start'");
+            if (para.Contains("begin"))         ans.Add("[" + para.IndexOf("begin") +"] Rather minify use 'begin' ");
+            if (para.Contains("just"))          ans.Add("[" + para.IndexOf("just") + "] Rather minify use 'just' ");
+            // https://qz.com/647121/five-weak-words-you-should-avoid-and-what-to-use-instead/
+            if (para.Contains("things")) ans.Add("[" + para.IndexOf("things") + "] Rather minify use 'things'. e.g. 'this article said a lot of things' with 'this article discussed the principles of...'");
+            if (para.Contains("stuff")) ans.Add("[" + para.IndexOf("stuff") + "] Rather minify use 'stuff'. e.g. 'this article said a lot of things' with 'this article discussed the principles of...'");
+            if (para.Contains("i believe")) ans.Add("[" + para.IndexOf("i believe") + "] Rather minify use 'I believe'. Doesn't make reader comfident. e.g. 'I believe the reasercher as a great point here' vs. 'the researcher has a great point here'");
+            if (para.Contains("i feel")) ans.Add("[" + para.IndexOf("i feel") + "] Rather minify use 'i feel'. believe e.g. 'I believe the reasercher as a great point here' vs. 'the researcher has a great point here'");
+            if (para.Contains("i think")) ans.Add("[" + para.IndexOf("i think") + "] Rather minify use 'i think'. believe e.g. 'I believe the reasercher as a great point here' vs. 'the researcher has a great point here' ");
+            if (para.Contains("what")) ans.Add("[" + para.IndexOf("what") + "] Rather minify use 'what'. Write in a more active like 'sally mailed the letter' instead of 'the letter was mailed by sally'");
+            if (para.Contains("is")) ans.Add("[" + para.IndexOf("is") + "] Rather minify use 'is'. Write in a more active like 'sally mailed the letter' instead of 'the letter was mailed by sally'");
+            if (para.Contains("are")) ans.Add("[" + para.IndexOf("are") + "] Rather minify use 'are'. Write in a more active like 'sally mailed the letter' instead of 'the letter was mailed by sally'");
+            if (para.Contains("am")) ans.Add("[" + para.IndexOf("am") + "] Rather minify use 'am'. Write in a more active like 'sally mailed the letter' instead of 'the letter was mailed by sally'");
+            if (para.Contains("very")) ans.Add("[" + para.IndexOf("very") + "] Rather minify use 'very'. e.g. 'scientists are very excited with...' with 'scientists are excited with...' is better");
+
+            // if (para.Contains("")) ans.Add("[" + para.IndexOf("") + "] Rather minify use '' ");
 
             var speller = RunLongSpellCheck(para);
             if (speller != null)
