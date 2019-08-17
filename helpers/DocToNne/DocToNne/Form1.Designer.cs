8a4d1a6a0dbb10e5f36e0d626784766ef51ba8a4
edg3 <loveland.ernest@gmail.com>
2019-08-17 08:44:15 +0200

    Helper added: DocToNne

---
+++ helpers/DocToNne/DocToNne/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DocToNne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butConvert
+            // 
+            this.butConvert.Location = new System.Drawing.Point(12, 12);
+            this.butConvert.Name = "butConvert";
+            this.butConvert.Size = new System.Drawing.Size(174, 23);
+            this.butConvert.TabIndex = 0;
+            this.butConvert.Text = "Convert to Nne";
+            this.butConvert.UseVisualStyleBackColor = true;
+            this.butConvert.Click += new System.EventHandler(this.ButConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 47);
+            this.Controls.Add(this.butConvert);
+            this.Name = "Form1";
+            this.Text = "DocToNne";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butConvert;
+    }
+}
+
---
+++ helpers/DocToNne/DocToNne/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocToNne
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButConvert_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialogue = new OpenFileDialog();
+            openFileDialogue.Filter = "Word Documents|*.doc; *.docx";
+
+            if (openFileDialogue.ShowDialog() == DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                var path = openFileDialogue.FileName;
+                var miss = System.Type.Missing;
+                Microsoft.Office.Interop.Word.Document wordDoc = wordApp.Documents.Open(path,
+                    miss, miss, miss, miss,
+                    miss, miss, miss, miss,
+                    miss, miss, miss, miss,
+                    miss, miss, miss);
+                
+                if (File.Exists("output.nne"))
+                {
+                    File.Delete("output.nne");
+                }
+
+                using (FileStream fileStream = new FileStream("output.nne", FileMode.Create))
+                {
+                    using (StreamWriter fileWriter = new StreamWriter(fileStream))
+                    {
+                        foreach (Paragraph p in wordDoc.Paragraphs)
+                        {
+                            var line = p.Range.Text.Trim();
+                            if (line.IndexOf("Ch. ") == 0)
+                            {
+                                fileWriter.WriteLine("p" + line);
+                            }
+                            else
+                            {
+                                fileWriter.WriteLine("P" + line);
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Complete", "File converted to 'output.nne'", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
