b33f43b64409ce0479db131c38e90d5547218472
edg3 <loveland.ernest@gmail.com>
2019-08-23 12:55:12 +0200

    removed litedb

--- src/NaNoE/FormMain.cs
+++ src/NaNoE/FormMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NaNoE.Objective;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -31,6 +32,8 @@ namespace NaNoE
         public FormMain()
         {
             InitializeComponent();
+
+            ObjectiveDB odb = new ObjectiveDB("test.db");
 
             ClearWeb();
             WebShowNovel();
---
+++ src/NaNoE/Objective/ObjectiveDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaNoE.Objective
+{
+    class ObjectiveDB
+    {
+        public ObjectiveDB(string fileName)
+        {
+
+        }
+    }
+}
