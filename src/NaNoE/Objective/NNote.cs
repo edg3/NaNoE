2c68f0e1ca966d5887fa29a60d71057b40cd2615
edg3 <loveland.ernest@gmail.com>
2019-08-23 12:34:42 +0200

    initial classes

---
+++ src/NaNoE/Objective/NNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaNoE.Objective
+{
+    class NNote
+    {
+        public string Value { get; set; }
+        public NNote(string val)
+        {
+            Value = val;
+        }
+    }
+}
---
+++ src/NaNoE/Objective/NPlot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaNoE.Objective
+{
+    class NPlot
+    {
+        public string Name { get; set; }
+        public NPlot(string name)
+        {
+            Name = name;
+        }
+
+        public List<NNote> Notes = new List<NNote>();
+    }
+}
