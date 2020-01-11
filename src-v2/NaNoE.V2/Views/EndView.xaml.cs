55fc1fccfdd23721ebcbac6dda53cfbd9c11ace5
Ernest <loveland.ernest@gmail.com>
2020-01-11 11:08:33 +0200

    fixed the UI interaction

--- src-v2/NaNoE.V2/Views/EndView.xaml.cs
+++ src-v2/NaNoE.V2/Views/EndView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using NaNoE.V2.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,25 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NaNoE.V2
+namespace NaNoE.V2.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for EndView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EndView : UserControl
     {
-        private static MainWindow _instance;
-        public static MainWindow Instance { get { return _instance; } }
-
-        public MainWindow()
+        public EndView()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            _instance = this;
-            DataContext = new StartingViewModel();
         }
     }
 }
--- src-v2/NaNoE.V2/Views/StartingView.xaml.cs
+++ src-v2/NaNoE.V2/Views/StartingView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using NaNoE.V2.Data;
+using NaNoE.V2.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,46 @@ namespace NaNoE.V2.Views
         public StartingView()
         {
             InitializeComponent();
+        }
+
+        private void butNew_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog();
+            sfd.FileName = "SQLite Novel";
+            sfd.DefaultExt = ".sqlite";
+            sfd.Filter = "SQLite Novels (.sqlite)|*.sqlite";
+            sfd.OverwritePrompt = false;
+
+            Nullable<bool> result = sfd.ShowDialog();
+
+            if (result == true)
+            {
+                if (File.Exists(sfd.FileName))
+                {
+                    MessageBox.Show("Can't use name of already created files.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    DBManager.Instance.Create(sfd.FileName);
+                    MainWindow.Instance.DataContext = new EndViewModel();
+                }
+            }
+        }
+
+        private void butOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.FileName = "SQLite Novel";
+            ofd.DefaultExt = ".sqlite";
+            ofd.Filter = "SQLite Novels (.sqlite)|*.sqlite";
+
+            Nullable<bool> result = ofd.ShowDialog();
+
+            if (result == true)
+            {
+                DBManager.Instance.Load(ofd.FileName);
+                MainWindow.Instance.DataContext = new EndViewModel();
+            }
         }
     }
 }
