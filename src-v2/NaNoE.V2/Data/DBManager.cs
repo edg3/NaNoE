ddb576eaca067ce5a89466fab94100b0e935f9e8
Ernest <loveland.ernest@gmail.com>
2020-01-18 16:12:47 +0200

    initial DB structures

--- src-v2/NaNoE.V2/Data/DBManager.cs
+++ src-v2/NaNoE.V2/Data/DBManager.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NaNoE.V2.Data
 {
+    /// <summary>
+    /// DB connection manager
+    /// </summary>
     class DBManager
     {
+        /// <summary>
+        /// Reference to the DB
+        /// </summary>
         private static DBManager _instance;
         public static DBManager Instance
         {
@@ -19,9 +26,123 @@ namespace NaNoE.V2.Data
             }
         }
 
+        /// <summary>
+        /// Instantiate
+        /// </summary>
         private DBManager()
         {
 
+        }
+
+        /// <summary>
+        /// Sqlite Connection
+        /// </summary>
+        private SQLiteConnection _connection;
+
+        /// <summary>
+        /// DB Connected
+        /// </summary>
+        public bool Connected { get { return _connection != null; } }
+
+        /// <summary>
+        /// Load a DB
+        /// </summary>
+        /// <param name="file">The .sqlite file to load</param>
+        public void Load(string file)
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+            }
+
+            _connection = new SQLiteConnection("Data Source=" + file);
+            _connection.Open();
+        }
+
+        /// <summary>
+        /// Create a .sqlite file
+        /// </summary>
+        /// <param name="file">Create a .sqlite file</param>
+        public void Create(string file)
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+            }
+
+            SQLiteConnection.CreateFile(file);
+
+            _connection = new SQLiteConnection("Data Source=" + file);
+            _connection.Open();
+
+            CreateTables();
+        }
+
+        /// <summary>
+        /// Create database tables
+        /// </summary>
+        private void CreateTables()
+        {
+            // 0 - Chapter
+            // 1 - Paragraph
+            // 2 - Note
+            // 3 - Bookmark
+
+            string elementTableCreate = "CREATE TABLE elements (id int identity(1,1), idbefore int, idafter int, type int, externalid int)";
+            ExecSQLNonQuery(elementTableCreate);
+
+            string paragraphTableCreate = "CREATE TABLE paragraphs (id int identity(1,1), content text, bool flagged)";
+            ExecSQLNonQuery(paragraphTableCreate);
+
+            string noteTableCreate = "CREATE TABLE notes (id int identity(1,1), content text)";
+            ExecSQLNonQuery(noteTableCreate);
+
+            string bookmarkTableCreate = "CREATE TABLE bookmarks (id int identity(1,1), content text)";
+            ExecSQLNonQuery(bookmarkTableCreate);
+        }
+
+        /// <summary>
+        /// Execute SQL without return
+        /// </summary>
+        /// <param name="sql">SQL to execute</param>
+        internal void ExecSQLNonQuery(string sql)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, _connection);
+            cmd.ExecuteNonQuery();
+        }
+
+        /// <summary>
+        /// Execute SQL with return
+        /// </summary>
+        /// <param name="sql">SQL to execute</param>
+        /// <param name="answerSize">Size of answers</param>
+        /// <returns>List of object[size] answers</returns>
+        internal List<object[]> ExecSQLQuery(string sql, int answerSize)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, _connection);
+            List<object[]> returns = new List<object[]>();
+            string[] line;
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            line = new string[answerSize];
+                            for (int i = 0; i < answerSize; i++)
+                            {
+                                line[i] = reader.GetString(i);
+                            }
+                            returns.Add(line);
+                        }
+                        catch { }
+                    }
+                }
+            }
+
+            return returns;
         }
     }
 }
