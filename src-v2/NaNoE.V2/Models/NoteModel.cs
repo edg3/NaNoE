d32451992f9aff68230ff19dab73f887e40c9888
Ernest <loveland.ernest@gmail.com>
2020-01-18 12:52:28 +0200

    added NoteModel

---
+++ src-v2/NaNoE.V2/Models/NoteModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaNoE.V2.Models
+{
+    class NoteModel : ModelBase
+    {
+        /// <summary>
+        /// Note's text
+        /// </summary>
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                Changed("Text");
+            }
+        }
+    }
+}
