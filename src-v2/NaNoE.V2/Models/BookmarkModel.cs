407b620376e0571e8bd80299ebac0d7323768f59
Ernest <loveland.ernest@gmail.com>
2020-01-18 12:54:47 +0200

    added BookmarkModel

---
+++ src-v2/NaNoE.V2/Models/BookmarkModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaNoE.V2.Models
+{
+    class BookmarkModel : ModelBase
+    {
+        /// <summary>
+        /// Bookmark's Text
+        /// </summary>
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                Changed("Text");
+            }
+        }
+    }
+}
