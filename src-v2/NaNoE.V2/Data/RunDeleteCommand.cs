7046a61ca3f16ca2c8398b335340a2b70314ea52
edg3 <loveland.ernest@gmail.com>
2020-03-15 11:24:42 +0200

    error note

--- src-v2/NaNoE.V2/Data/RunDeleteCommand.cs
+++ src-v2/NaNoE.V2/Data/RunDeleteCommand.cs
@@ -20,6 +20,7 @@ namespace NaNoE.V2.Data
         public void Execute(object parameter)
         {
             // Note: perhaps give more info that what it is/was?
+            // Error: crash when we delete an element in midnovel very close to endnovel view level?
             var answer = MessageBox.Show("Are you sure you want to delete that element?\n\n" + DBManager.Instance.GetID((int)parameter).ModelType, "Confirm Delete", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
             if (answer == MessageBoxResult.Yes)
