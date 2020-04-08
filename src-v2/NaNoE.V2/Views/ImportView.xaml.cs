6317ea0f535392208e653d692408ffb34ce5c130
edg3 <loveland.ernest@gmail.com>
2020-04-08 06:49:04 +0200

    started import view

--- src-v2/NaNoE.V2/Views/ImportView.xaml.cs
+++ src-v2/NaNoE.V2/Views/ImportView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NaNoE.V2.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,39 @@ namespace NaNoE.V2.Views
         public ImportView()
         {
             InitializeComponent();
+        }
+
+        private void butImportDocX_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void butCreateSqlite_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void butRunImport_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblImportDocX.Content.ToString() == "")
+            {
+                MessageBox.Show("Please select a DocX to import.");
+                return;
+            }
+            if (lblCreateSqlite.Content.ToString() == "")
+            {
+                MessageBox.Show("Please let us know the '.sqlite' to create.");
+                return;
+            }
+
+            MessageBox.Show("Please note: this could be a long process if the word document is very long, just leave NaNoE running for it to complete.");
+
+            throw new NotImplementedException();
+        }
+
+        private void butBack_Click(object sender, RoutedEventArgs e)
+        {
+            Navigator.Instance.Goto("start");
         }
     }
 }
